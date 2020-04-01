429119b74d3ee110c0f26ec18b1447b7f8126f19
philip r brenan <philiprbrenan@gmail.com>
2020-04-02 00:06:36 +0100

    Vita_on_2020_04_02_at_00_06_36

--- github/uploadToGit.pl
+++ github/uploadToGit.pl
@@ -9,15 +9,21 @@ use Carp;
 use Data::Dump qw(dump);
 use Data::Table::Text qw(:all);
 
-my $home = q(/home/phil/vita/minimum);
+my $home = q(/home/phil/vita/minimum);                                          # Home folder
+my $perl = fpd($home, qw(github));                                              # Upload to github
+my $p1   = fpe($perl, qw(uploadOut pl));
+my $pa   = fpe($perl, qw(a out));
+my $p2   = fpe($perl, qw(uploadOut perl));
 
-my @f = searchDirectoryTreesForMatchingFiles($home, qw(.py .pl .perl .yml));
+lll qx(pp -I /home/phil/perl/cpan/GitHubCrud/lib $p1; mv $pa $p2);              # Package uploader
+
+my @f = searchDirectoryTreesForMatchingFiles($home, qw(.py .pl .perl .yml));    # Files we want to upload
 lll "Files:\n", dump([@f]);
 for my $f(@f)
  {lll qx(git add $f);
  }
 
-my $date = dateTimeStampName;
+my $title = q(Vita).dateTimeStampName;                                          # Name for commit
 
-lll qx(git commit -m "Vita$date");
-lll qx(git push -u origin master);
+lll qx(git commit -m "$title");
+lll qx(git push -u origin master);                                              # Push to GitHub via SSH
