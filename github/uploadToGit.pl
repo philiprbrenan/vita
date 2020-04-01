4249290b53f0c6663b62e9ca546480b95a54ae26
philip r brenan <philiprbrenan@gmail.com>
2020-04-01 23:58:21 +0100

    _on_2020_04_01_at_23_58_21

---
+++ github/uploadToGit.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -I/home/phil/perl/cpan/DataTableText/lib/
+#-------------------------------------------------------------------------------
+# Upload all files to GitHub  _
+# Philip R Brenan at gmail dot com, Appa Apps Ltd Inc., 2020
+#-------------------------------------------------------------------------------
+use warnings FATAL => qw(all);
+use strict;
+use Carp;
+use Data::Dump qw(dump);
+use Data::Table::Text qw(:all);
+
+my $home = q(/home/phil/vita/minimum);
+
+my @f = searchDirectoryTreesForMatchingFiles($home, qw(.py .pyc .pl .perl .yml));
+say STDERR "AAAA ", dump([@f]);
+for my $f(@f)
+ {lll qx(git add $f);
+ }
+
+my $date = dateTimeStampName;
+
+lll qx(git commit -m "$date");
+lll qx(git push -u origin master);
---
+++ tests/2Dprojections/test1b-HFS-midplane.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 18:28:22 2019
+
+@author: Daniel.Ibanez
+"""
+from sol_heat_flux.eich import Eich
+import numpy as np
+
+footprint = Eich(lambda_q=2.5e-3, S=0.5e-3, r0_lfs=0.75, r0_hfs=0.2)# lambda_q=2.5, S=0.5
+
+x=np.linspace(-1, 10, 1000)*1e-3
+footprint.s_disconnected_dn_max = 1.0e-3
+footprint.f_x_in_out = 5.0
+footprint.set_coordinates(x*footprint.f_x_in_out)
+q_0 = 1e6
+
+footprint.calculate_heat_flux_density("hfs-mp")
+footprint.plot_heat_power_density()
+print(footprint._s_disconnected_dn_inboard)
+print(q_0*footprint.calculate_heat_power()*2*np.pi*footprint.r0_hfs)
